99e76142fee88d95466a91648641dc060305147a
Tiziana Cammilli <tiziana.cammilli@gmail.com>
2016-10-20 11:28:19 +0200

    lvt

--- areas/suggerimenti/lavatrici.php
+++ areas/suggerimenti/lavatrici.php
@@ -40,7 +40,6 @@
 	<p>
 		Come conseguenza di un lembo di guarnizione attorcigliata vi &egrave; il rischio di perdite d'acqua!
 	</p>
-	
 </div>
 
 <h3>GUARNIZIONE</h3>
@@ -48,10 +47,14 @@
 	<br>
 	<b>La guarnizione è sporca</b>
 	<p>
-		I costanti lavaggi a temperature basse nonché l'uso eccessivo di detersivi liquidi (a base di enzimi) possono causare la formazione di morchia e residui gelatinosi sulla guarnizione dell'oblò, sui tubi e sul cestello.<br>
-		I tubi si ostruiscono e nella lavatrice si sviluppano cattivi odori che possono persino lasciare residui sul bucato.<br>
-		La causa potrebbe essere l'uso di eccessivo detersivo o lavaggi regolari a basse temperature.<br>
-		Se si effettuano soltanto lavaggi a massimo 40 °C, col tempo si formerà un accumulo di residui di detersivo e sporco nella lavatrice.<br>
+		I costanti lavaggi a temperature basse nonché l'uso eccessivo di detersivi liquidi (a base di enzimi) possono causare la formazione di morchia e residui gelatinosi sulla guarnizione dell'oblò, sui tubi e sul cestello.
+		<br>
+		I tubi si ostruiscono e nella lavatrice si sviluppano cattivi odori che possono persino lasciare residui sul bucato.
+		<br>
+		La causa potrebbe essere l'uso di eccessivo detersivo o lavaggi regolari a basse temperature.
+		<br>
+		Se si effettuano soltanto lavaggi a massimo 40 °C, col tempo si formerà un accumulo di residui di detersivo e sporco nella lavatrice.
+		<br>
 		Questo fenomeno può essere la causa di sporcizia, macchie o grasso sulla guarnizione.
 	</p>
 	<p>
@@ -80,13 +83,124 @@
 <h3>MALFUNZIONAMENTI</h3>
 <div>
 	<br>
-	<b>UNO</b>
+	<b>Sul display della lavatrice viene visualizzato l'errore E20</b>
 	<p>
-		<br>
+		L'errore E20 indica che la lavatrice non ha scaricato l'acqua. Il filtro sporco, o la presenza di corpi estranei, può impedire lo scarico.
 	</p>
 	<p>
-		<i>Per determinare l'esatta natura del problema, è opportuno che uno dei nostri tecnici venga ad ispezionare la macchina a domicilio.</i>
+		Seguendo le indicazioni riportate sul libretto istruzioni provvedere alla pulizia del filtro. Se la lavatrice scarica direttamente a muro, smontare e pulire a fondo il raccordo di scarico a muro.
 	</p>
+	<p>
+		<i>e il problema persiste richiedere l'intervento di un tecnico. </i>
+	</p>
+	<br>
+	<b>Sul display della lavatrice viene visualizzato l'errore E40</b>
+	<p>
+		L'oblò della lavatrice non è ben chiuso, aprire e richiudere l'oblò dopo aver verificato che un carico eccessivo non ne impedisca la completa chiusura.
+	</p>
+	<p>
+		<i>Se il problema persiste richiedere l'intervento di un tecnico. </i>
+	</p>
+	<br>
+	<b>Sul display della lavatrice viene visualizzato l'errore EF0</b>
+	<p>
+		Il detersivo utilizzato potrebbe aver prodotto schiuma in eccesso che la lavatrice non riesce ad espellere.
+	</p>
+	<p>
+		Accertarsi che il detersivo usato sia idoneo per lavatrice e provare ad effettuare un nuovo lavaggio utilizzando metà della quantità raccomandata di detersivo.
+	</p>
+	<br>
+	<b>Sul display della lavatrice lampeggia una spia</b>
+	<p>
+		Il LED può stare ad indicare che lo sportello è aperto o che il pulsante di avvio non è stato premuto.
+	</p>
+	<p>
+		Innanzitutto effettuare queste verifiche. Se si tratta di un errore, il relativo significato è riportato nel manuale delle istruzioni.
+	</p>
+	<p>
+		<i>Se il problema persiste  per determinare l'esatta natura del problema, è opportuno che uno dei nostri tecnici autorizzati venga ad ispezionare la macchina a domicilio </i>
+	</p>
+	<br>
+	<b>Il display della lavatrice non &egrave; leggibile</b>
+	<p>
+		Se il display non è leggibile, potrebbe essere dovuto al fatto che non lo si sta guardando frontalmente. Se si guarda il display lateralmente o dall'alto verso il basso, questo può sembrare poco chiaro.
+		La regolazione della luminosità e del contrasto del display possono inoltre avere un effetto sulla sua leggibilità.
+		Se la lavatrice è dotata della funzione di regolazione della luminosità e del contrasto, è possibile trovare ulteriori informazioni nel manuale delle istruzioni.
+	</p>
+	<p>
+		<i>Se quanto detto sopra non risolve il problema, è possibile richiedere l'intervento di un tecnico.</i>
+	</p>
+	<br>
+	<b>La lavatrice ha strappato i capi</b>
+	<p>
+		I problemi riscontrati possono avere cause differenti.
+		I simboli di lavaggio presenti sui capi indicano se possono essere sottoposti a centrifuga? Capi non idonei ad essere centrifugati inevitabilmente si rompono.
+		È possibile che l'apparecchiatura sia stata sovraccaricata? I bottoni e le cerniere sono stati chiusi durante il lavaggio?
+		Sono visibili dei corpi estranei nella guarnizione dello sportello? Sono presenti segni di danneggiamento nel cestello?
+	</p>
+	<p>
+		<i>Per ulteriore assistenza e per determinare l'esatta natura del problema, è opportuno che uno dei nostri tecnici autorizzati venga ad ispezionare la macchina a domicilio </i>
+	</p>
+	<p>
+		La lavatrice fa rumore - si muove e vibra durante la centrifuga
+	</p>
+	<p>
+		<ol>
+			<li>
+				Assicurarsi che i bulloni per il trasporto siano stati rimossi dal retro dell'apparecchiatura.
+			</li>
+			<li>
+				Livellare l’apparecchiatura agendo sui piedini regolabili. L'apparecchiatura deve essere stabile e posizionata su un pavimento rigido e in piano. Se necessario, servirsi di una livella. Le eventuali regolazioni possono essere eseguite con una chiave inglese. È importante che non vi siano dei "rimbalzi" poiché l'apparecchiatura vibra durante la centrifuga
+			</li>
+			<li>
+				Assicurarsi che tutti i tubi flessibili siano montati e fissati correttamente e in modo sicuro alla parte posteriore del corpo dell'apparecchiatura. I tubi allentati possono vibrare contro la macchina durante i cicli di centrifuga emettendo un rumore simile a dei colpi.
+			</li>
+			<li>
+				Controllare che la biancheria da lavare sia stata caricata correttamente assicurandosi che nessun tessuto rimanga intrappolato tra lo sportello e il cestello.
+			</li>
+			<li>
+				Inserire un unico solo capo di biancheria o sovraccaricare il cestello può in ugual modo causare una centrifuga sbilanciata.
+			</li>
+			<li>
+				Monete, accendini, bottoni o altri piccoli oggetti intrappolati nel filtro di scarico; controllare il filtro se la macchina ne contiene uno che sia visibile. Consultare il manuale delle istruzioni per capire come eseguire questa operazione.
+			</li>
+			<li>
+				Ferretti dei reggiseni – controllare i tubi flessibili del cestello/tra le sezioni del cestello
+			</li>
+		</ol>
+	</p>
+	<p>
+		vIL PROGRAMMA DI LAVAGGIO DELLA LAVATRICE DURA TROPPO A LUNGO
+	</p>
+	<p>
+		Un carico di biancheria sbilanciato aumenta la durata del programma. Non si tratta di un'anomalia della lavatrice. La lavatrice calcola il tempo in base al carico di biancheria e il display ne visualizza una stima. L'aumento graduale della durata del programma riduce il consumo di energia. La fase di riscaldamento ottimizzata permette di risparmiare energia e, la durata più lunga, garantisce gli stessi risultati di lavaggio (soprattutto in caso di sporco normale). Se nessuna delle situazioni di cui sopra chiarisce il problema, e se si ha accesso alla presa, eseguire un reset della lavatrice staccando la spina, attendere 30 secondi quindi ricollegarla.
+	</p>
+	<p>
+		<i>Se il problema persiste, si consiglia l'intervento di uno dei nostri tecnici autorizzati</i>
+	</p>
+	<p>
+		vLA LAVATRICE NON SCALDA E NON COMPAIONO CODICI DI ERRORE
+	</p>
+	<p>
+		L'elettronica dell'apparecchiatura è in grado di riconoscere un guasto della resistenza e di produrre il relativo codice di errore.In caso contrario, il problema potrebbe essere facilmente risolvibile. Seguire le istruzioni seguenti per capire se i suggerimenti forniti aiutano a risolvere il problema.
+	</p>
+	<ol>
+		<li>
+			Controllare che l'apparecchiatura non sia sovraccarica
+		</li>
+		<li>
+			Verificare che il programma selezionato preveda un ciclo a caldo.
+		</li>
+		<li>
+			Il filtro dello scarico è stato pulito di recente?
+		</li>
+		<li>
+			Se l'acqua della zona è dura, ai carichi di lavaggio può essere necessario aggiungere delle pastiglie anticalcare. È possibile acquistare tali pastiglie nel nostro negozio.
+		</li>
+		<li>
+			Eseguire un lavaggio di servizio e riavviare il ciclo con del detersivo nuovo.
+		</li>
+	</ol>
 </div>
 
 <h3> ODORI SGRADEVOLI</h3>
@@ -115,21 +229,140 @@
 	<p>
 		<i>Se queste soluzioni non risolvono il problema, provare a utilizzare il nostro prodotto detergente per lavatrici, seguendo le raccomandazioni indicate.</i>
 	</p>
+</div>
+
+<h3>PULIZIA DELLA LAVATRICE</h3>
+<div>
+	<p>
+		<i><b>Prima di eseguire qualunque intervento di manutenzione, spegnere l'apparecchiatura ed estrarre la spina dalla presa elettrica. Non scollegare alcun componente elettrico o funzionale, senza prima aver consultato Electrolux in quanto ciò potrebbe invalidare la garanzia.</b></i>
+	</p>
+	<p>
+		Si consiglia di pulire l'apparecchiatura semplicemente con acqua e sapone. Infatti i detergenti possono reagire con gli ammorbidenti, graffiando i componenti di plastica e rendendoli fragili. Vedere anche le Istruzioni utente per la manutenzione dell'apparecchiatura. Le informazioni riportate di seguito servono da guida:
+	</p>
+	<p>
+		Avviare un ciclo di lavaggio a 95 °C con detergente per lavatrice per evitare la formazione di odori e di macchie intorno allo sportello. 
+	</p>
+	<p>
+		Per la corretta pulizia del filtro, seguire le istruzioni riportate di seguito:
+		<ol>
+			<li>
+				Spegnere l'apparecchiatura;
+			</li>
+			<li>
+				Aprire lo sportello del vano filtro e posizionarvi sotto un contenitore raccogli acqua;
+			</li>
+			<li>
+				Scaricare l'acqua dalla macchina aprendo i tubi flessibili di scarico accanto al filtro;
+			</li>
+			<li>
+				Rimuovere il filtro ruotandolo (con fermezza) in senso antiorario;
+			</li>
+			<li>
+				Pulire il filtro e l'alloggiamento della pompa sciacquandoli sotto l'acqua corrente, quindi reinstallare il filtro;
+			</li>
+			<li>
+				Chiudere il tubo flessibile di scarico e riposizionarlo nella sua posizione originale;
+			</li>
+			<li>
+				Chiudere lo sportello del vano filtro.
+			</li>
+		</ol>
+		L'apparecchiatura è ora pronta all'uso; all'avvio del ciclo verificare la presenza eventuale di perdite.
+	</p>
+	<p>
+		<i>Le informazioni di cui sopra sono fornite esclusivamente come indicazioni approssimative. È anche possibile fare riferimento alle istruzioni complete presenti  nel libretto di uso e manutenzione.</i>
+	</p>
+	<p>
+		Pulizia del cassetto del detersivo
+	</p>
+	<br>
+	Nel tempo possono formarsi funghi e muffe. Pulire eventuali fuoriuscite o residui di detersivo dal cassetto ponendolo sotto acqua corrente tiepida.
+	Estrarre il cassetto e utilizzare uno spazzolino per pulire i residui e le incrostazioni di sapone in polvere o liquido dalle singole sezioni (un vecchio spazzolino può andar bene).
+	</p>
+	<p>
+		Pulizia della guarnizione della lavatrice.
+	</p>
+	<br>
+	<p>
+		La muffa che si forma sulle guarnizioni in gomma delle lavatrici tende a macchiarle di scuro. È possibile rimuovere tali muffe usando del bicarbonato di sodio, della candeggina gel o del detergente per lavastoviglie, i quali sciolgono anche le macchie e i residui di sapone più ostinati. Durante la pulizia assicurarsi di indossare dei guanti in gomma e degli occhiali protettivi.
+	</p>
+	<p>
+		Esterno
+	</p>
+	<br>
+	<p>
+		Spesso le lavatrici presentano una finitura esterna smaltata. La soluzione migliore è utilizzare un panno umido o imbevuto di detergente universale delicato o di sapone per i piatti. Pulire delicatamente la superficie esterna della lavatrice. È possibile riscontrare la formazione di evidenti macchie appiccicose che i pelucchi e i residui di sapone per bucato hanno formato sulla lavatrice. Di solito, un panno imbevuto di acqua tiepida è sufficiente per pulire l'intera zona. Per la pulizia degli angoli e degli spigoli, procedere con un cotton fioc.
+	</p>
+	<br>
+	<b><i>NON UTILIZZARE PRODOTTI ABRASIVI.</i></b>
+</div>
+
+<h3> PROBLEMI RELATIVI AL CALCARE</h3>
+<div>
+	<br>
+	<p>
+		Se a causa dell'acqua dura si forma del calcare, una buona soluzione sarebbe aggiungere dell' anticalcare al detersivo per lavatrice oppure installare un dispositivo anticalcare magnetico come il nostro prodotto NEOCAL.
+	</p>
 	<div class="left-box">
-		<img src="areas/suggerimenti/cleancare.jpg"/>
+		<img src="areas/suggerimenti/neocal.jpg"/>
 		<div class="text-box">
-			<b>Tenete la vostra lavatrice pulita e fresca come se fosse nuova</b>
 			<br>
 			<p>
-				I costanti lavaggi a temperature basse nonché il crescente uso di detergenti liquidi (a base di enzimi) può causare la formazione di morchia e residui gelatinosi sulla guarnizione dell'oblò, sui tubi e sul cestello.
+				Dispositivo Universale Anticalcare ai magneti permanenti al NEODIMIO, da applicare al rubinetto della lavatrice o Lavastoviglie.
 				<br>
-				I tubi si ostruiscono, e si sviluppano cattivi odori che possono persino lasciare residui sul vostro bucato.
+				Riduce il rischio di incrostazioni assicurando una maggiore durata dell'elettrodomestico.
 				<br>
-				Questo kit rimuove tutte le sostanze residue, lasciando pulite tutte le componenti della lavatrice e garantendo condizioni di pulizia ottimali.
-				<br>
-				Questa confezione consente di effettuare 2 applicazioni.
+				Il dispositivo possiede magneti permanenti con un potente campo magnetico che rompe i cristalli di calcio, trasformandoli in aragonite, impedendo l’aggregazione e quindi l’incrostazione.
 			</p>
 		</div>
 		</p>
+		<br>
+		<p>
+			Per i consigli sulla pulizia, non dovrebbe essere necessario richiedere l'intervento di un tecnico a meno che l'apparecchiatura presenti un guasto.
+		</p>
+		<p>
+			Per ulteriore assistenza fare riferimento al manuale delle istruzioni; invece, per i prodotti di pulizia raccomandati visitare il nostro negozio.
+		</p>
 	</div>
+	<p>
+		<b>Come rimuovere il calcare dalla mia macchina?</b>
+		<br>
+	<p>
+		L'interno del cestello della macchina può sembrare bello lucido, ma spesso, sulla parte posteriore non immediatamente visibile, può depositarsi del calcare.
+	</p>
+	<p>
+		Il calcare depositato sulla resistenza della macchina può compromettere l'apparecchiatura in vari modi:
+		<ul>
+			<li>
+				I tempi per il riscaldamento dell'acqua aumentano.
+			</li>
+			<li>
+				Il consumo di corrente aumenta.
+			</li>
+			<li>
+				La durata della resistenza si riduce notevolmente e, al peggio, pu&oacute; bruciarsi.
+			</li>
+			<li>
+				I cuscinetti principali della macchina potrebbero danneggiarsi
+			</li>
+		</ul>
+		<br>
+	</p>
+	<p>
+		Il calcare è causato dalla "durezza" dell'acqua. Più è dura l'acqua, più è probabile che la macchina accumuli calcare. La durezza dell'acqua varia da zona a zona.
+	</p>
+	<p>
+		Questo accade perché le varie zone ricevono acqua da diverse sorgenti idriche, in funzione del consumo di corrente. Visitare il sito web del proprio ente locale di fornitura di servizi idrici per verificare l'attuale durezza dell'acqua presso la propria abitazione. Si consiglia di utilizzare l'anticalcare Electrolux. Insieme al prodotto vengono fornite le istruzioni per il dosaggio e il manuale delle istruzioni.
+	</p>	
+	<div class="left-box">
+		<img src="areas/suggerimenti/descaler.jpg"/>
+		<div class="text-box">
+			<br>
+			<p>
+				Rimuove il calcare per consentire alla lavatrice e alla lavastoviglie di funzionare come se fossero nuove.
+				<br>
+				Questo decalcificante è ideale per le zone in cui l'acqua è più dura, dato che consente di rimuovere il depositi di calcare: ciò consente di aumentare la durata di vita dell'apparecchiatura evitando la formazione di cattivi odori.
+				<br>
+				Offre prestazioni ottimali e riduce il consumo energetico: mantenendo puliti i tubi e il cestello vengono garantiti dei risultati di lavaggio ottimali con un consumo minimo di energia, ciclo dopo ciclo.
+			</p>
 </div>
